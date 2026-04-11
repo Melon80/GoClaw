2f5231ec0139335dffbbe578599544289c61c7dd
Melon80 <2597090588@qq.com>
2026-04-11 13:57:41 +0800

    完全覆盖 lgm_test 分支内容

--- start-gateway.ps1
+++
@@ -1,7 +0,0 @@
-$p = Start-Process -FilePath "D:\opencode\GoClaw\picoclaw.exe" -ArgumentList "gateway" -PassThru
-Start-Sleep -Seconds 3
-if (-not $p.HasExited) {
-    Write-Host "Running - PID: $($p.Id)"
-} else {
-    Write-Host "Exited with code: $($p.ExitCode)"
-}
